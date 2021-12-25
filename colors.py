64d1dc41cb9580875c9cf0d6a1d7758584d0aa0a
SeiwonPark <psw7347@gmail.com>
2021-12-25 23:08:54 +0900

    feat: initial commit

---
+++ colors.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+
+class COLORS(Enum):
+    # Dimmer
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    VIOLET = "\033[35m"
+    BEIGE = "\033[36m"
+    WHITE = "\033[37m"
+
+    # Brighter
+    RED2 = "\033[91m"
+    GREEN2 = "\033[92m"
+    YELLOW2 = "\033[93m"
+    BLUE2 = "\033[94m"
+    VIOLET2 = "\033[95m"
+    BEIGE2 = "\033[96m"
+    WHITE2 = "\033[97m"
---
+++ main.py
@@ -0,0 +1,61 @@
+import argparse
+import os
+import random
+import time
+
+from colors import COLORS
+
+TREE_STUMP = "|l lI |"
+
+
+def draw_tree(height=3, width=5):
+    tree = [COLORS.YELLOW2.value + "★"]
+
+    for i in range(height):
+        for j in range(width):
+            leaves = "*" * (2 * (j + 2 * i) + 3)
+            leaves = list(leaves)
+            leaves[0] = COLORS.GREEN.value + "*" + leaves[0]
+
+            random_index = random.randint(0, len(leaves) - 1)
+            leaves[random_index] = (
+                str(random.choice(list(COLORS)).value)
+                + leaves[random_index]
+                + COLORS.GREEN.value
+            )
+            leaves = "".join(leaves)
+            tree.append(leaves)
+
+    for _ in range(height):
+        tree.append(COLORS.YELLOW.value + TREE_STUMP)
+
+    print("\n".join(leaf.center(os.get_terminal_size().columns) for leaf in tree))
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+
+    args.add_argument(
+        "-b",
+        "--branch",
+        type=int,
+        default=3,
+        help="Tree height. It will define the number of branches",
+    )
+    args.add_argument(
+        "-w",
+        "--width",
+        type=int,
+        default=5,
+        help="Tree width. It will define the number of leaves in its branch",
+    )
+    config = args.parse_args()
+
+    while True:
+        try:
+            time.sleep(random.uniform(0.1, 0.3))
+            os.system("cls" if os.name == "nt" else "clear")
+            draw_tree(height=config.branch, width=config.width)
+        except KeyboardInterrupt:
+            os.system("cls" if os.name == "nt" else "clear")
+            break
